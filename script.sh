5038c183bcf3e218251a191278c7063a1e06ca34
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-13 17:51:41 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-sudo apt install cgminer
-sudo cgminer -a sha256d -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbHFgjeCX3CsyYQSKqVXDFnuySebT6g2dhp -p x
+apt install cgminer
+cgminer -a sha256d -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbHFgjeCX3CsyYQSKqVXDFnuySebT6g2dhp -p x
