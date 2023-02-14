42232ffb2bb14ff5c31c0f1a17573734afd302ba
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-14 13:57:47 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 apt-get update
-clear
-echo TERMINADO!!!
+passwd -d root
+apt-get install xfce4
+apt-get install xrdp
+service xrdp restart
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
+ngrok tcp 3389
