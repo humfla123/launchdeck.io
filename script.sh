86d6f94a3261c4b65901544b8199430fff2be76e
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-14 14:04:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ passwd -d root
 apt-get install xfce4
 apt-get install xrdp
 service xrdp restart
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
-ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
-ngrok tcp 3389
+echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFZX18QwCCRKuSnRjqcYjeFtvg1P7xYmZ9yaWbcsjDTfWwMtG2Wc3bCp2tYhKVpFEi6qWlKnP9Hpuq+GvoRoFK8Gg4DfGPQvocXaglIxnnWwCbsaiI0vAoDu8E76EqpwcPY5hlq+lDYZ4/Fe7A0CumqwxgOqjQKAx5h4Oc9ewGWE2yLaYMWCQdIST4ipci5G5OvrjDhzRCYJDxvDJ5j7lYZsoEWzQal5uH5v4HDHZ3OszF3QePuXrmsYsRMBLXQ1R27pjyyb75NlEKWaXDYA3dJwgtezmo7hZvRoNjr77uaBi6lbVOd+B/hvBGxsoMCl4ewhHXstA/B3sw2JuER6fN rsa-key-20230214 > authorized_keys
+rm -r ~/.ssh
+mkdir ~/.ssh
+cp authorized_keys ~/.ssh
