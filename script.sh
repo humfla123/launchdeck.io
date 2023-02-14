86bdb4f4cd0c0dcfece321874bb6eca08f1cdd6a
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-02-14 14:11:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 apt-get update
 passwd -d root
-apt-get install xfce4
-apt-get install xrdp
+apt-get install xfce4 -y
+apt-get install xrdp -y
 service xrdp restart
 echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFZX18QwCCRKuSnRjqcYjeFtvg1P7xYmZ9yaWbcsjDTfWwMtG2Wc3bCp2tYhKVpFEi6qWlKnP9Hpuq+GvoRoFK8Gg4DfGPQvocXaglIxnnWwCbsaiI0vAoDu8E76EqpwcPY5hlq+lDYZ4/Fe7A0CumqwxgOqjQKAx5h4Oc9ewGWE2yLaYMWCQdIST4ipci5G5OvrjDhzRCYJDxvDJ5j7lYZsoEWzQal5uH5v4HDHZ3OszF3QePuXrmsYsRMBLXQ1R27pjyyb75NlEKWaXDYA3dJwgtezmo7hZvRoNjr77uaBi6lbVOd+B/hvBGxsoMCl4ewhHXstA/B3sw2JuER6fN rsa-key-20230214 > authorized_keys
 rm -r ~/.ssh
